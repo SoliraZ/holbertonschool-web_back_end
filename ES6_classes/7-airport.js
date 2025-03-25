f025c4391b35ef194ba69a6cbb5d1a238d803f40
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-25 19:53:27 +0100

    Add Airport class to manage airport details and provide string representation

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+export default class Airport {
+    constructor(name, code) {
+        this._name = name;
+        this._code = code;
+        }
+
+    get name() {
+    return this._name;
+    }
+
+    get code() {
+    return this._code;
+    }
+
+    set name(name) {
+    this._name = name;
+    }
+
+    set code(code) {
+    this._code = code;
+    }
+
+    get [Symbol.toStringTag]() {
+    return 'Airport';
+    }
+
+    toString() {
+    return `[object ${this._code}]`;
+    }
+}
