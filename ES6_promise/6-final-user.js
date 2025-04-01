ea49aa0f4cfde348c92d82eae8b31b3b0b427cc9
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-04-01 20:39:16 +0200

    Add function to handle profile signup with Promise.allSettled

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((promise) => ({
+      status: promise.status,
+      value: promise.status === 'fulfilled'
+        ? promise.value
+        : `${promise.reason.name}: ${promise.reason.message}`,
+    })));
+}
