12eae72bfaf5f9b5b95230c897687d90b78b4b21
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-04-23 16:41:33 +0200

    Add make_multiplier function to create a multiplier function

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Function that take a float multiplier and return a function that multiplies a float by multiplier."""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by multiplier."""
+    def multiply(n: float) -> float:
+        """Return the product of n and multiplier."""
+        return n * multiplier
+    return multiply
