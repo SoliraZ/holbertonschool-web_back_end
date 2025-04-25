979d7bd1ceb0f3e9f7ca48ec769c473a434d36d8
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-04-25 12:20:17 +0200

    Refactor docstring in measure_time function for clarity

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Measure the runtime of wait_n(n, max_delay) and return the average time."""
+    """Measure the runtime of wait_n and return the average time."""
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
