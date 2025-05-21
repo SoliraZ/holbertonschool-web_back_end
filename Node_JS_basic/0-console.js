142fadef5a0b17067f22d0019dcdfec623f697fe
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-05-21 12:52:47 +0200

    Update documentation for displayMessage function to clarify parameter usage

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,6 @@
 /**
  * This is a simple Node.js module that exports a function to display a message.
+ * @param {string} message - The message to be displayed.
  */
 
 function displayMessage(message) {
