3ebe3f4ca893dc317eace2f0d40e7b76f0e2ce4d
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-05-22 11:44:10 +0200

    Remove unnecessary comments and documentation from console.js

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,11 +1,5 @@
-/**
- * This is a simple Node.js module that exports a function to display a message.
- * @param {string} message - The message to be displayed.
- */
-
 function displayMessage(message) {
     console.log(message);
 }
 
-// Export the function so it can be used in other files
 module.exports = displayMessage;
