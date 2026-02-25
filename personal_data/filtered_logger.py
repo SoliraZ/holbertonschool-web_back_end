8451d7ab5751cea69da7ed73da47da95b516bcf5
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-02-25 15:59:52 +0100

    Refactor filter_datum function in filtered_logger.py to improve type hints and update docstring for clarity

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""Utilities for obfuscating sensitive personal data in log messages."""
+"""Utilities for obfuscating personal data in log messages."""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """Obfuscate specified fields in a log message."""
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Return `message` with values of `fields` replaced by `redaction`."""
     pattern = rf"({'|'.join(map(re.escape, fields))})=[^{separator}]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
