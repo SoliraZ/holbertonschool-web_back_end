1887e7ad324a222ee5ef574b948eb11951c036fe
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-02-25 17:02:04 +0100

    Add get_logger function to filtered_logger.py for creating a configured logger that redacts sensitive user data.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,9 @@ import re
 from typing import List
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Return `message` with values of `fields` replaced by `redaction`."""
@@ -28,3 +31,15 @@ class RedactingFormatter(logging.Formatter):
         """Format the record and redact configured sensitive fields."""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Return a configured logger for user data."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+        logger.addHandler(handler)
+    return logger
