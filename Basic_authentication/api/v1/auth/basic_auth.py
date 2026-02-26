7b8c4b49073f490eb9bf25de95a5095df3047cca
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-02-26 22:14:24 +0100

    Add user_object_from_credentials method to BasicAuth class for retrieving User instance based on email and password

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 """Basic authentication module
 """
 import base64
-from api.v1.auth.auth import Auth
+from api.v1.auth.auth import Auth, User
+from models.user import User as UserModel
 
 
 class BasicAuth(Auth):
@@ -53,3 +54,26 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> User:
+        """Retrieves a User instance based on email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = UserModel.search({'email': user_email})
+        except Exception:
+            return None
+
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
