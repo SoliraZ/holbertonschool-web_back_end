28b9b002c3e87ed43c7fe81d04d255f4a0fb7a1b
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-02-26 21:48:04 +0100

    Add request filtering and authentication checks in app.py; enhance authorization header retrieval in Auth class.

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,13 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Get the authorization header from the request
         """
-        return None
+        if request is None:
+            return None
+
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> User:
         """Get the current user from the request
