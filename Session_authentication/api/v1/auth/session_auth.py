58dbd4913ae056a33f1826dbcfdb40bd89703a5d
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-03-05 12:30:24 +0100

    Add create_session method to SessionAuth class for generating session IDs and mapping them to user IDs.

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,23 @@
 """Session authentication module
 """
 
+from uuid import uuid4
+
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Session authentication class that inherits from Auth
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a given user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
