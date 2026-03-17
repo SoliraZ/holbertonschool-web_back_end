210f082b8464bc8d0eee41f45d2bf31901942566
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-03-17 14:40:40 +0100

    Add user registration endpoint to Flask app, allowing users to register with email and password, and handle errors for existing emails.

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,12 @@
 """Flask app module
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -14,6 +19,18 @@ def root() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """Register a new user
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
