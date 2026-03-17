5102d779099ed911d1f50832ee8e97555966282a
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-03-17 16:00:08 +0100

    Add update_password method to Auth class for securely updating user passwords using reset tokens, improving account recovery process.

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -91,3 +91,18 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update user's password based on reset token
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed = _hash_password(password).decode("utf-8")
+        self._db.update_user(
+            user.id,
+            hashed_password=hashed,
+            reset_token=None,
+        )
