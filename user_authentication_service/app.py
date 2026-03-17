a9f9633e5a133e08a686eb4a068f17f5c6f34ba6
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-03-17 15:06:06 +0100

    Add login route to Flask app for user authentication, allowing users to log in with email and password, and setting a session cookie upon successful login.

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Flask app module
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 from auth import Auth
 
@@ -31,6 +31,21 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": email, "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """Login route
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
