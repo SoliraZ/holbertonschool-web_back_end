9b00abcbe433f96a9cc42625e8331f586e950f99
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-04-01 16:29:15 +0200

    Add call history decorator to store method in Cache class
    - Applied the `call_history` decorator to the `store` method in the Cache class.
    - This enhancement allows for tracking the inputs and outputs of the method, improving observability and debugging capabilities.
    - Supports better performance analysis and data integrity checks for interactions with Redis.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -42,6 +42,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis using a random key."""
